5f17ce19fb8128ecc44da1e47a5812ee566a22e4
Nimit <nimitmore27@gmail.com>
2022-06-24 15:05:11 +0530

    txtbox focus property , bg change ,checkbox effect

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
 const ToDoItems = document.getElementsByClassName("to_do_items")[0];
 const input = document.getElementById("input");
 const trashIcon = document.getElementById("trash");
-input.addEventListener("keydown",function(event){
-    if(event.key === "Enter")
-    {
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
         addItem();
     }
 });
-function addItem(){
-    var divParent = document.createElement("div");
-    var divChild = document.createElement("div");
-    var checkIcon = document.createElement("i");
-    var trashIcon = document.createElement("i");
-    divParent.className = "item";
-    divParent.innerHTML = "<div id='strike'>"+input.value+"</div>";
-    checkIcon.className = "fas fa-check-square";
-    checkIcon.style.color = "lightgray";
-    checkIcon.addEventListener("click",function(){
-        checkIcon.style.color = "limegreen";
-        let temp = document.getElementById("strike").innerHTML;
-        document.getElementById("strike").innerHTML = "<strike>"+temp+"</strike>";
-    })
-    divChild.appendChild(checkIcon);
-    trashIcon.className = "fas fa-trash";
-    trashIcon.style.color = "darkgray";
-    trashIcon.addEventListener("click",function(){
-        divParent.remove();
-    })
-    divChild.appendChild(trashIcon);
-    divParent.appendChild(divChild);
-    ToDoItems.appendChild(divParent);
-    input.value = '';
+function addItem() {
+    if (input.value === '') {
+        alert("Please Enter A Task !");
+        document.getElementById('input').focus();
+    } else {
+        let divParent = document.createElement("div");
+        let divChild = document.createElement("div");
+        let checkIcon = document.createElement("i");
+        let trashIcon = document.createElement("i");
+        divParent.className = "item";
+        divParent.innerHTML = "<div>" + input.value + "</div>";
+        checkIcon.className = "fas fa-check-square";
+        checkIcon.style.color = "darkgray";
+        checkIcon.addEventListener("click", function () {
+            if (checkIcon.style.color === 'limegreen') {
+                checkIcon.style.color = 'darkgray'
+                divParent.style.textDecoration = 'none';
+            } else {
+                checkIcon.style.color = "limegreen";
+                divParent.style.textDecoration = 'line-through';
+            }
+        })
+        divChild.appendChild(checkIcon);
+        trashIcon.className = "fas fa-trash";
+        trashIcon.style.color = "darkgray";
+        trashIcon.addEventListener("click", function () {
+            divParent.remove();
+        })
+        divChild.appendChild(trashIcon);
+        divParent.appendChild(divChild);
+        ToDoItems.appendChild(divParent);
+        input.value = '';
+        document.getElementById("input").focus();
+    }
+
 }
